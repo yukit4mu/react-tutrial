9b85ccbd4f4a070267dae0af9f2ddfba72de478b
Yukit4mu <yua_kitamura@decogn.com>
2024-08-25 10:45:04 +0900

    delete: all files

--- src/TodoList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Todo from './Todo'
-
-// コンポーネント
-const TodoList = ({todos}) => {
-  return todos.map((todo) => <Todo todo = {todo} key={todo} />);
-};
-// 出力
-export default TodoList
